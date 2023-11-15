133a9e52d3a051170d30ca0364c95047016de8d6
Pol Carulla <65311355+PolCarulla@users.noreply.github.com>
2023-11-16 00:24:11 +0100

    Modified init version 1
    Added MAX TURN, AVALIABLE COLORS and validateSecretCode

--- game/models/mastermindModel.py
+++ game/models/mastermindModel.py
@@ -1,6 +1,20 @@
+from enum import Enum
+  
+    
 class MasterMindModel:
+    AVAILABLE_COLORS = ["Red", "Green", "Blue", "Yellow","Orange","Purple"]
+    MAX_TURN = 6
+    
     def __init__(self,code):
+        self.validateSecretCode(code)
         self.secretCode = code
+        self.turn = 0
+        self.gameEnded = False
 
+    def validateSecretCode(self,code:list):
+        for color in code:
+            if color not in self.AVAILABLE_COLORS:
+                raise ValueError(f"Invalid color in secret code: {color}. Available colors: {self.AVAILABLE_COLORS}")
+            
     def checkGuess(self,guess):
         pass
--- tests/models/mastermindTestModel.py
+++ tests/models/mastermindTestModel.py
@@ -7,7 +7,23 @@ class TestMasterMindModel(unittest.TestCase):
         mm = MasterMindModel(code)
         self.assertEqual(mm.secretCode,code)
         
+    def test_init2(self):
+        code = ["Red","Blue","Purple","Orange"]
+        mm = MasterMindModel(code)
+        self.assertEqual(mm.secretCode,code)
+        self.assertEqual(0,mm.turn)
+        self.assertEqual(["Red", "Green", "Blue", "Yellow","Orange","Purple"],mm.AVAILABLE_COLORS)
+        self.assertEqual(6,mm.MAX_TURN)
+        self.assertFalse(mm.gameEnded)
         
+    def test_validateSecretCode(self):
+        valid_secret_code = ["Red", "Green", "Blue", "Yellow"]
+        invalid_secret_code = ["Red", "Cyan", "Blue", "Yellow"]
+        
+        mm = MasterMindModel(valid_secret_code)
+        
+        with self.assertRaises(ValueError):
+            mm = MasterMindModel(invalid_secret_code)
         
         
     def test_checkGuess(self):
