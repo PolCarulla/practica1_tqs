4c99aab85690fb14c41e974b1ee9ca55b140b67a
pol <pol@ymaging.com>
2023-11-20 15:48:40 +0100

    Init version 3 with comments on tests

--- game/models/mastermindModel.py
+++ game/models/mastermindModel.py
@@ -1,20 +1,25 @@
-from enum import Enum
-  
-    
+
 class MasterMindModel:
     AVAILABLE_COLORS = ["Red", "Green", "Blue", "Yellow","Orange","Purple"]
     MAX_TURN = 6
-    
-    def __init__(self,code):
-        self.validateSecretCode(code)
+    def __init__(self,code=None,available_colors=None):
+        if available_colors != None:
+            self.AVAILABLE_COLORS = available_colors
+        
+        if code != None:
+            self.validateSecretCode(code)
         self.secretCode = code
+
         self.turn = 0
         self.gameEnded = False
 
     def validateSecretCode(self,code:list):
+        if len(code) != 4:
+            raise IndexError(f"Invalid lenght of secret code. Expected 4 and got {len(code)}")
         for color in code:
             if color not in self.AVAILABLE_COLORS:
                 raise ValueError(f"Invalid color in secret code: {color}. Available colors: {self.AVAILABLE_COLORS}")
+        
             
     def checkGuess(self,guess):
         pass
--- tests/models/mastermindTestModel.py
+++ tests/models/mastermindTestModel.py
@@ -3,19 +3,35 @@ from game.models.mastermindModel import MasterMindModel
 
 class TestMasterMindModel(unittest.TestCase):
     def test_init(self):
+        ## Starting init test funcion
+        ## Testing if model init with code as an argument stores it correctly
         code = ["Red","Blue","Purple","Orange"]
         mm = MasterMindModel(code)
         self.assertEqual(mm.secretCode,code)
         
     def test_init2(self):
+        ## Second version of init funcion
+        ## Added QoL variables such as starting turn to keep track of the game, available colors, max game turns and a bool to check if the game has ended 
         code = ["Red","Blue","Purple","Orange"]
         mm = MasterMindModel(code)
-        self.assertEqual(mm.secretCode,code)
         self.assertEqual(0,mm.turn)
         self.assertEqual(["Red", "Green", "Blue", "Yellow","Orange","Purple"],mm.AVAILABLE_COLORS)
         self.assertEqual(6,mm.MAX_TURN)
         self.assertFalse(mm.gameEnded)
-        
+    
+    def test_init3(self):
+        ## Third version of init function
+        ## Available colors can now be passed as an argument to the init function, default colors should be kept if not used
+        ## Secret code can be ommitted during object init, setting it's value to None and skipping validateSecretCode
+        available_colors = ["Red", "Green", "Blue", "Yellow","Black","White"]
+
+        mm = MasterMindModel()
+        self.assertIsNone(mm.secretCode)
+        self.assertEqual(["Red", "Green", "Blue", "Yellow","Orange","Purple"],mm.AVAILABLE_COLORS)
+
+        mm = MasterMindModel(available_colors=available_colors)
+        self.assertEqual(available_colors,mm.AVAILABLE_COLORS)
+
     def test_validateSecretCode(self):
         valid_secret_code = ["Red", "Green", "Blue", "Yellow"]
         invalid_secret_code = ["Red", "Cyan", "Blue", "Yellow"]
