426b57affcfceb741e4182252b55dc7af98dae13
pol <pol@ymaging.com>
2023-11-23 13:46:53 +0100

    Start menu version 2. Added Difficulty Enum

--- game/controllers/mastermindController.py
+++ game/controllers/mastermindController.py
@@ -1,6 +1,15 @@
+from enum import Enum
+
+class Difficulty(Enum):
+    EASY = 1
+    MEDIUM = 2
+    HARD = 3
+    CRAZY = 4
+
 class MasterMindController:
     model = None
     view = None
+    difficulty = Difficulty.EASY
 
     def setView(self,view):
         self.view = view
@@ -9,7 +18,15 @@ class MasterMindController:
         self.model = model
         
     def start_menu(self):
-        self.view.displayMenu()
+        while True:
+            self.view.displayMenu(self.difficulty)
+            user_input = self.view.userInput()
+            if user_input == "start_game":
+                self.run_game()
+            elif user_input == "exit":
+                break
+            else:
+                raise KeyError(f"Unexpected input {user_input}")
 
     def runGame(self):
         pass
--- tests/controllers/mastermindTestController.py
+++ tests/controllers/mastermindTestController.py
@@ -1,10 +1,12 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock,call
 
 from game.controllers.mastermindController import MasterMindController
 from game.models.mastermindModel import MasterMindModel
+from game.controllers.mastermindController import Difficulty
 
 class TestMasterMindController(unittest.TestCase):
+    @unittest.skip("Changed start_menu, using start_menu2")
     def test_start_menu(self):
         ## First start menu test
         ## View display menu should be called
@@ -16,7 +18,30 @@ class TestMasterMindController(unittest.TestCase):
 
         self.assertEqual(mock_view,mc.view)
         self.assertTrue(mock_view.displayMenu.called)
-        
+    
+    def test_start_menu2(self):
+        ## Second version start menu
+        ## Start menu needs to be called passing the difficulty level, starting always at easy
+        ## Waits for a user input:
+        ### If input is "start_game" function run_game should be called
+        ### If input is "exit" game should be closed
+        ### Any other input needs to raise a keyValue error
+        ## The function needs to stay in a loop waiting for "start_game" or "exit" calls.
+        mc = MasterMindController()
+        mock_view = Mock()
+        mock_view.userInput.return_value = "exit"
+        mc.setView(mock_view)
+        mc.start_menu()
+
+        expected_arguments = Difficulty.EASY
+
+        mock_view.displayMenu.assert_has_calls([call(expected_arguments)])
+        mock_view.userInput.assert_called_once()
+
+        mock_view.userInput.side_effect = ["aaaa",KeyError("Unexpected input aaaa")]
+        with self.assertRaises(KeyError):
+            mc.start_menu()
+    
     def test_run_game(self):
         pass
 
